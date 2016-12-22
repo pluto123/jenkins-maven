cda4be926024d803bb4a5b96703615e0b21da935
pluto123 <yuh.chang.wu@gmail.com>
2016-12-22 08:21:01 +0800

    Update TestBankAccount.java

--- lab/src/test/java/tw/com/pluto/TestBankAccount.java
+++ lab/src/test/java/tw/com/pluto/TestBankAccount.java
@@ -1,15 +1,1 @@
-package tw.com.pluto;
 
-import junit.framework.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-public class TestBankAccount {
-    
-    @Test
-    public void testDebitWithSufficientFounds() {
-        BankAccount account = new BankAccount(10);
-        double amount = account.debit(5);
-        Assert.assertEquals(5.0, amount);
-    }
-}
